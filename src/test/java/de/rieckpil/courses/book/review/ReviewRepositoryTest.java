25466030e6fe46cbfcd1d7652fde5d9e88689db4
Futó Tamás <futo.tamas@webstar.hu>
2024-02-05 17:49:29 +0100

    Testing the persistence layer - @DataJpaTest - In-memory database (H2)

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
@@ -21,7 +21,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
   "spring.datasource.driver-class-name=com.p6spy.engine.spy.P6SpyDriver", // P6Spy
   "spring.datasource.url=jdbc:p6spy:h2:mem:testing;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false" // P6Spy
 })
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+/**
+ * @see spy.properties
+ */
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // P6Spy miatt fent megadtuk a DS-t, ezért nem kell azt automatán létrehozni
 class ReviewRepositoryTest {
 
   @Autowired
@@ -36,11 +39,46 @@ class ReviewRepositoryTest {
   @Autowired
   private TestEntityManager testEntityManager;
 
+  @BeforeEach
+  void beforEach() {
+    // Default behavior is rolling back every change after a test execution, so every test should start on an empty database
+    assertEquals(0, cut.count());
+  }
+
   @Test
   void notNull() throws SQLException {
+    assertNotNull(entityManager);
+    assertNotNull(cut);
+    assertNotNull(dataSource);
+    assertNotNull(testEntityManager);
+
+    System.out.println("Database used by test is " + dataSource.getConnection().getMetaData().getDatabaseProductName());
+
+    Review review = new Review();
+    review.setBook(null);
+    review.setContent("Great book!");
+    review.setRating(4);
+    review.setUser(null);
+    review.setTitle("Great!");
+    review.setCreatedAt(LocalDateTime.now());
+
+    cut.save(review);
+
+    assertNotNull(review.getId());
   }
 
   @Test
   void transactionalSupportTest() {
+    Review review = new Review();
+    review.setBook(null);
+    review.setContent("Great book!");
+    review.setRating(4);
+    review.setUser(null);
+    review.setTitle("Great!");
+    review.setCreatedAt(LocalDateTime.now());
+
+    cut.save(review);
+
+    assertNotNull(review.getId());
   }
 }
