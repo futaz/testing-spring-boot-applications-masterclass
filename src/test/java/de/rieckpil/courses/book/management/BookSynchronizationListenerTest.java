211d2db867bfeb035667cbc738f776be0f8cfcbf
Futó Tamás <futo.tamas@webstar.hu>
2024-02-05 17:46:42 +0100

    Mocking with Mockito

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -21,25 +21,57 @@ class BookSynchronizationListenerTest {
   private OpenLibraryApiClient openLibraryApiClient;
 
   @InjectMocks
-  private BookSynchronizationListener cut;
+  private BookSynchronizationListener cut; // class under test
 
   @Captor
   private ArgumentCaptor<Book> bookArgumentCaptor;
 
   @Test
   void shouldRejectBookWhenIsbnIsMalformed() {
+    BookSynchronization sync = new BookSynchronization("42");
+    cut.consumeBookUpdates(sync);
+    verifyNoInteractions(openLibraryApiClient, bookRepository);
   }
 
   @Test
   void shouldNotOverrideWhenBookAlreadyExists() {
+    BookSynchronization sync = new BookSynchronization("1234567890123");
+    when(bookRepository.findByIsbn("1234567890123")).thenReturn(new Book());
+    cut.consumeBookUpdates(sync);
+    verifyNoInteractions(openLibraryApiClient);
+    verify(bookRepository, times(0)).save(ArgumentMatchers.any());
   }
 
   @Test
   void shouldThrowExceptionWhenProcessingFails() {
+    BookSynchronization sync = new BookSynchronization("1234567890123");
+    when(bookRepository.findByIsbn("1234567890123")).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook("1234567890123")).thenThrow(new RuntimeException("Network timeout"));
+    assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(sync));
   }
 
   @Test
   void shouldStoreBookWhenNewAndCorrectIsbn() {
+    BookSynchronization sync = new BookSynchronization(VALID_ISBN);
+    Book book = new Book();
+    book.setTitle("Java book");
+    book.setIsbn(VALID_ISBN);
+
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenReturn(book);
+    when(bookRepository.save(ArgumentMatchers.any())).then(invocation -> {
+      Book methodArg = invocation.getArgument(0);
+      methodArg.setId(1L);
+      return methodArg;
+    });
+
+    cut.consumeBookUpdates(sync);
+
+    verify(openLibraryApiClient).fetchMetadataForBook(VALID_ISBN);
+    verify(bookRepository).save(bookArgumentCaptor.capture());
+
+    assertEquals("Java book", bookArgumentCaptor.getValue().getTitle());
+    assertEquals(VALID_ISBN, bookArgumentCaptor.getValue().getIsbn());
   }
 
 }
