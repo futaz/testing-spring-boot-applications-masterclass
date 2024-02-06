9ebc0f11b5147f7b49f16302342541e2f054ab54
Futó Tamás <futo.tamas@webstar.hu>
2024-02-06 16:53:31 +0100

    Testing the web layer - Access to Role-Based Secured Endpoints

--- src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
@@ -18,8 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -141,10 +140,23 @@ class ReviewControllerTest {
 
   @Test
   void shouldNotAllowDeletingReviewsWhenUserIsAuthenticatedWithoutModeratorRole() throws Exception {
+    mockMvc.perform(
+      delete("/api/books/{isbn}/reviews/{reviewId}", 42, 100)
+        .with(jwt()))
+      .andExpect(status().isForbidden());
+
+    verifyNoInteractions(reviewService);
   }
 
   @Test
   @WithMockUser(roles = "moderator")
   void shouldAllowDeletingReviewsWhenUserIsAuthenticatedAndHasModeratorRole() throws Exception {
+    mockMvc.perform(
+        delete("/api/books/{isbn}/reviews/{reviewId}", 42, 100)
+//          .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_moderator")))
+      )
+      .andExpect(status().isOk());
+
+    verify(reviewService).deleteReview("42", 100L);
   }
 }
