dde03111519f27ac1cb279f1e7c797f9e911b83d
Futó Tamás <futo.tamas@webstar.hu>
2024-02-06 15:29:15 +0100

    Testing the web layer - Verifying the First Behavior

--- src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
@@ -4,22 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import de.rieckpil.courses.config.WebSecurityConfig;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ReviewController.class)
 // see https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.7-Release-Notes#migrating-from-websecurityconfigureradapter-to-securityfilterchain
@@ -34,8 +32,29 @@ class ReviewControllerTest {
 
   private ObjectMapper objectMapper;
 
+  @BeforeEach
+  void beforeEach() {
+    objectMapper = new ObjectMapper();
+  }
+
   @Test
   void shouldReturnTwentyReviewsWithoutAnyOrderWhenNoParametersAreSpecified() throws Exception {
+    final ArrayNode array = objectMapper.createArrayNode();
+
+    final ObjectNode book = objectMapper.createObjectNode();
+    book.put("bookId", 1);
+    book.put("isbn", "42");
+    book.put("avg", 89.3);
+    book.put("ratings", 2);
+
+    array.add(book);
+
+    when(reviewService.getAllReviews(20, "none")).thenReturn(array);
+
+    mockMvc
+      .perform(get("/api/books/reviews"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.size()", is(1)));
   }
 
   @Test
